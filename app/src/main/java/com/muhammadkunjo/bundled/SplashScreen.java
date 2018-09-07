eab0c6fdb627e34b771ebbe4fd47f8a16fcfee9f
shaotsu <mkunjo121@gmail.com>
2018-09-07 19:20:59 -0400

    Initial commit

---
+++ app/src/main/java/com/muhammadkunjo/bundled/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.muhammadkunjo.bundled;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i= new Intent(SplashScreen.this,MainActivity.class);//Start Another Activity
+                startActivity(i);
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
