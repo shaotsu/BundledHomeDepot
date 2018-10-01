b988cc41ed4aaa068d8cbba258cbdf8d54a362c5
shaotsu <mkunjo121@gmail.com>
2018-10-01 01:30:24 -0400

    New Firebase DB for login & registration

--- app/src/main/java/com/muhammadkunjo/bundled/LoginActivity.java
+++ app/src/main/java/com/muhammadkunjo/bundled/LoginActivity.java
@@ -2,21 +2,36 @@ package com.muhammadkunjo.bundled;
 
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity{
 
     ConstraintLayout logLayout;
     AnimationDrawable animationDrawable;
-    private Button signin;
-    private Button createacc;
-    private Button skip;
+    private Button button_signin;
+    private Button button_register;
+    private Button button_skip;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+
 
 
     @Override
@@ -26,11 +41,57 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(this, "Welcome to #FE6601's app!", Toast.LENGTH_LONG).show();
         logLayout = (ConstraintLayout) findViewById(R.id.logLayout);
 
+        mAuth = FirebaseAuth.getInstance();
+
         animationDrawable = (AnimationDrawable) logLayout.getBackground();
         animationDrawable.setEnterFadeDuration(4500);
         animationDrawable.setExitFadeDuration(4500);
         animationDrawable.start();
+
+        //link variables to their xml IDs
+        button_register = (Button) findViewById(R.id.login_button_register);
+        button_signin = (Button) findViewById(R.id.login_button_signin);
+        editTextEmail = (EditText) findViewById(R.id.login_email);
+        editTextPassword = (EditText) findViewById(R.id.login_password);
     }
+
+    //Login
+    public void loginUser(View view){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required to login");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email address");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    Toast.makeText(getApplicationContext(), "Login Successful. Welcome!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+    //Go to Register Activity
     public void gotoRegister(View view) {
         startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
     }
